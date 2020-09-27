916215a81fa6103cb40b2546f4f64503e2d2c7ed
Seth White <sethewhite@me.com>
2020-09-27 19:28:52 -0400

    Updates

---
+++ precache-manifest.4e64acce8cc12696b9460ef5dcef748e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8864e0f82308c2699f9147228d4974",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a1e732bf868184e8ca92",
+    "url": "/monsters-rolodex/static/css/main.3c64587e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1e732bf868184e8ca92",
+    "url": "/monsters-rolodex/static/js/main.fc099acb.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
